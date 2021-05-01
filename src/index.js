73472aad68a8f032c60ce25fadd951dbc427f354
nishy <nishyjana6435@gmail.com>
2021-05-01 17:02:57 +0530

    jwt token generated after log in and verified and had a implemented a secured route

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
 const mysqlConnection = require('./connection/mysql')
-const mongoConnection = require('./connection/mongodb');
-
-mongoConnection();
-
-
+const mongoConnection = require('./connection/mongodb')
+const post = require('./routes/post')
 const authRoute = require('./routes/auth')
+
+mongoConnection()
 
 app.use(express.json())
 
 app.use('/api/user', authRoute)
+app.use('/api/post',post)
 
 app.listen(3000, () =>
   console.log('Server is Running.....................................')
